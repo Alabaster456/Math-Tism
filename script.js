cfd5d33f5443daf5133c859a38c4db0be7e2ff5d
Yazid <Alabaster456@users.noreply.github.com>
2014-04-18 10:32:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+ $(document).ready(function(){
+   $('#background').fadeIn('slow');
+  $('#M').delay(1000).fadeIn('slow');
+   $('#A').delay(1800).fadeIn('slow');
+  $('#T').delay(2300).fadeIn('slow');
+   $('#H').delay(2600).fadeIn('slow');
+$('#line').delay(2900).fadeIn('slow');
+   $('#T2').delay(3200).fadeIn('slow');
+  $('#I').delay(3400).fadeIn('slow');
+   $('#S').delay(3700).fadeIn('slow');
+  $('#M2').delay(3900).fadeIn('slow');
+  $('#start').delay(5000).fadeIn('slow');
+$('#Help').delay(5000).fadeIn('slow'); $('#firework1').delay(5000).fadeIn('slow');
+   $('#firework2').delay(5000).fadeIn('slow');
+ 
+ });
+
+            
